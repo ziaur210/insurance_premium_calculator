d09fe7846c40638dde8d11d9c296734a945dc0b0
ziaur210 <ziacseku210@gmail.com>
2019-07-23 18:03:32 +1000

    commit Webapi

---
+++ tal_premium/Webapi/Controllers/BaseapiController.cs
@@ -0,0 +1,10 @@
+﻿using System.Web.Http;
+
+namespace Webapi.Controllers
+{
+    // [Authorize]
+    public abstract class BaseapiController : ApiController
+    {
+        // Manage application  security check here and other common functionalities
+    }
+}
---
+++ tal_premium/Webapi/Controllers/CustomerController.cs
@@ -0,0 +1,39 @@
+﻿using BusinessObject;
+using DataManager;
+using DataManager.Managers;
+using System.Collections.Generic;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace Webapi.Controllers
+{
+
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    [RoutePrefix("api/customer")]
+    public class CustomerController : BaseapiController 
+    {
+        IManager<Customer> _manager = new CustomerManager();  
+             
+        [HttpGet]   
+        [Route("List/{searchKey}")]
+        public virtual IList<Customer> List(string searchKey)
+        {
+            IList<Customer> items = _manager.GetAll(searchKey);
+            return items;
+        }
+        [HttpGet]
+        [Route("List")]
+        public virtual IList<Customer> List()
+        {
+            IList<Customer> items = _manager.GetAll("");
+            return items;
+        }
+
+        [HttpPost]
+        public virtual int Add(Customer item)
+        {
+            int newItemId = _manager.Add(item);
+            return newItemId;
+        }
+    } // end of class
+}
