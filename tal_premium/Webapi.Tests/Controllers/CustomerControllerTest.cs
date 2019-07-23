ef0e559c3a7967cf7ad7cb7064473da8a7ae5cc6
ziaur210 <ziacseku210@gmail.com>
2019-07-23 18:05:45 +1000

    Commit Webapi.Tests project

---
+++ tal_premium/Webapi.Tests/Controllers/CustomerControllerTest.cs
@@ -0,0 +1,86 @@
+﻿using BusinessObject;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Webapi.Controllers.Tests
+{
+    [TestClass()]
+    public class CustomerControllerTest
+    {
+        Mock<CustomerController> mockingController = null;
+
+        public CustomerControllerTest()
+        {
+            mockingController = new Mock<CustomerController>();
+        }
+
+        [TestMethod()]
+        public void Test_List()
+        {
+            // arrange
+            string searchKey = "";
+            int expectedCount = 2;
+            int actualCount = 0;
+            mockingController.Setup(x => x.List(It.IsAny<string>())).Returns(GetList());
+            // Act
+            try
+            {
+                actualCount = mockingController.Object.List(searchKey).Count();
+            }
+            catch (Exception ex)
+            {
+                string errMsg = ex.Message;
+                actualCount = 0;
+            }
+            // Assert
+            Assert.IsTrue(expectedCount == actualCount);
+
+        }
+
+        [TestMethod()]
+        public void AddTest()
+        {
+            // arrange
+            
+            mockingController.Setup(x => x.Add(It.IsAny<Customer>())).Returns(1);
+            BusinessObject.Customer projectToAdd = new BusinessObject.Customer()
+            {
+                Name = "customer1"
+            };
+            // Act
+            int newLocationID = 0;
+            try
+            {
+                newLocationID =  mockingController.Object.Add(projectToAdd);
+            }
+            catch (Exception)
+            {
+                newLocationID = 2;
+            }
+            // Assert
+            Assert.IsTrue(newLocationID == 1);
+        }
+
+        #region Miscellaneous
+        private IList<Customer> GetList()
+        {
+            var list = new List<Customer>() { new Customer()
+            {
+                Id = 1,
+                Name = "customer1"               
+            },
+            new Customer()
+            {
+                Id = 2,
+                Name = "customer2",
+            },
+        };
+            return list;
+        }
+
+        #endregion Miscellaneous
+    }
+}
---
+++ tal_premium/Webapi.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Webapi.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Webapi.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c207315d-861f-4950-9109-494a90aeac89")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
